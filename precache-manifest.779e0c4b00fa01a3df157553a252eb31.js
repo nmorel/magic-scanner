d85c7117e92b6c166dcca2d37b180bda60a2ca6d
Nicolas Morel <nicolas.morel@klaxoon.com>
2019-05-20 19:28:58 +0200

    Updates

---
+++ precache-manifest.779e0c4b00fa01a3df157553a252eb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eaccefdbd0c836668de33a197d978e9",
+    "url": "/magic-scanner/index.html"
+  },
+  {
+    "revision": "bccdf2d79e5c75b67934",
+    "url": "/magic-scanner/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/magic-scanner/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bccdf2d79e5c75b67934",
+    "url": "/magic-scanner/static/js/main.99d9f3ac.chunk.js"
+  },
+  {
+    "revision": "4afd7088fbe89112aee1",
+    "url": "/magic-scanner/static/js/runtime~main.455a4907.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/magic-scanner/static/media/logo.5d5d9eef.svg"
+  }
+]);
